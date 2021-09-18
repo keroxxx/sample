e86107affdcd28f8e09b8697a6337c19b648702d
keroxxx <aknckannnn@gmail.com>
2021-09-19 01:28:14 +0900

    修正 #11

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,14 +5,15 @@ require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
   if Rails.env.production?
     config.fog_provider = 'fog/aws'
-    config.fog_directory  = 'sample-avatar'
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: ENV['AWS_ACCESS_KEY'],
-      aws_secret_access_key: ENV['AWS_SECRET_KEY'],
+      aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
       region: 'ap-northeast-1',
       path_style: true
     }
+    config.fog_directory  = 'sample-avatar'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/sample-avatar'
   else
     config.storage :file
     config.enable_processing = false if Rails.env.test?
